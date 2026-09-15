4b7381a9d46a62f94e55e014592694528ff9ff29
eric1050811-sc11 <eric1050811.sc11@nycu.edu.tw>
2026-09-16 00:27:12 +0800

    Lab1 Obj1 finish

--- Lab1/lab1/tests/riscv/riscv.mk
+++ Lab1/lab1/tests/riscv/riscv.mk
@@ -13,40 +13,40 @@ riscv_srcs += \
   riscv-ori.S \
   riscv-sw.S \
   
-  # riscv-jr.S \
-  # riscv-and.S \
-  # riscv-andi.S \
-  # riscv-beq.S \
-  # riscv-bge.S \
-  # riscv-bgeu.S \
-  # riscv-bltu.S \
-  # riscv-div.S \
-  # riscv-divu.S \
-  # riscv-j.S \
-  # riscv-jalr.S \
-  # riscv-lb.S \
-  # riscv-lbu.S \
-  # riscv-lh.S \
-  # riscv-lhu.S \
-  # riscv-mul.S \
-  # riscv-or.S \
-  # riscv-rem.S \
-  # riscv-remu.S \
-  # riscv-sb.S \
-  # riscv-sh.S \
-  # riscv-sll.S \
-  # riscv-slli.S \
-  # riscv-slt.S \
-  # riscv-slti.S \
-  # riscv-sltiu.S \
-  # riscv-sltu.S \
-  # riscv-sra.S \
-  # riscv-srai.S \
-  # riscv-srl.S \
-  # riscv-srli.S \
-  # riscv-sub.S \
-  # riscv-xor.S \
-  # riscv-xori.S \
+  riscv-jr.S \
+  riscv-and.S \
+  riscv-andi.S \
+  riscv-beq.S \
+  riscv-bge.S \
+  riscv-bgeu.S \
+  riscv-bltu.S \
+  riscv-div.S \
+  riscv-divu.S \
+  riscv-j.S \
+  riscv-jalr.S \
+  riscv-lb.S \
+  riscv-lbu.S \
+  riscv-lh.S \
+  riscv-lhu.S \
+  riscv-mul.S \
+  riscv-or.S \
+  riscv-rem.S \
+  riscv-remu.S \
+  riscv-sb.S \
+  riscv-sh.S \
+  riscv-sll.S \
+  riscv-slli.S \
+  riscv-slt.S \
+  riscv-slti.S \
+  riscv-sltiu.S \
+  riscv-sltu.S \
+  riscv-sra.S \
+  riscv-srai.S \
+  riscv-srl.S \
+  riscv-srli.S \
+  riscv-sub.S \
+  riscv-xor.S \
+  riscv-xori.S
   # TODO: riscv-mulh.S \
   # TODO: riscv-mulhu.S \
   # TODO: riscv-mulhsu.S \
---
+++ Lab1/lab1_report.md
@@ -0,0 +1,8 @@
+# Lab1 Report
+
+## Objective 1
+
+1. Finish `lab1/riscvstall/riscvstall-CoreCtrl.v` line 407 to 469 missing part.
+2. Compile the test vhm file, edit `lab1/test/riscv/riscv.mk`, uncomment the ones you want to test.
+
+Tips: uncomment all in `lab1/test/riscv/riscv.mk`, build all the test vhm files at once. Enable the tests you want in `lab1/build/Makefile` after modifying `lab1/riscvstall/riscvstall-CoreCtrl.v`
